6d36597c02a137ab9627311515ca7fff6e993d7b
zhs <zhs1@meitu.com>
2018-11-07 10:40:51 +0800

    fixed db keys

--- db/kv.go
+++ db/kv.go
@@ -2,8 +2,9 @@ package db
 
 import (
 	"bytes"
-	"encoding/json"
 	"log"
+
+	"gitlab.meitu.com/platform/thanos/db/store"
 )
 
 // Kv supplies key releated operations
@@ -17,18 +18,27 @@ func GetKv(txn *Transaction) *Kv {
 }
 
 // Keys iterator all keys in db
-func (kv *Kv) Keys(onkey func(key []byte) bool) error {
-	mkey := MetaKey(kv.txn.db, nil)
+func (kv *Kv) Keys(start []byte, filter func(key []byte) bool) error {
+	mkey := MetaKey(kv.txn.db, start)
+	prefixKey := MetaKey(kv.txn.db, nil)
 	iter, err := kv.txn.t.Seek(mkey)
 	if err != nil {
 		return err
 	}
+	defer iter.Close()
+
+	now := Now()
 	for iter.Valid() {
 		key := iter.Key()
-		if !bytes.HasPrefix(key, mkey) {
+		if !bytes.HasPrefix(key, prefixKey) {
 			break
 		}
-		if !onkey(key[len(mkey):]) {
+
+		obj, err := DecodeObject(iter.Value())
+		if err != nil {
+			return err
+		}
+		if !IsExpired(obj, now) && !filter(key[len(prefixKey):]) {
 			break
 		}
 		if err := iter.Next(); err != nil {
@@ -40,36 +50,45 @@ func (kv *Kv) Keys(onkey func(key []byte) bool) error {
 
 // Delete specific keys, ignore if non exist
 func (kv *Kv) Delete(keys [][]byte) (int, error) {
-	n := 0
-	for _, key := range keys {
-		mkey := MetaKey(kv.txn.db, key)
+	count := 0
+	now := Now()
+	metaKeys := make([][]byte, len(keys))
+	for i, key := range keys {
+		metaKeys[i] = MetaKey(kv.txn.db, key)
+	}
 
-		obj, err := kv.txn.Object(key)
-		if err != nil {
-			if err == ErrKeyNotFound {
+	dataBytes, err := store.BatchGetValues(kv.txn.t, metaKeys)
+	if err != nil {
+		return count, err
+	}
+	for i, data := range dataBytes {
+		if data != nil {
+			obj, err := DecodeObject(data)
+			if err != nil {
+				return count, err
+			}
+			if IsExpired(obj, now) {
 				continue
 			}
-			return -1, err
-		}
-
-		switch obj.Type {
-		case ObjectString:
-			if err := kv.txn.t.Delete(mkey); err != nil {
-				return -1, err
+			if err := kv.txn.Destory(obj, keys[i]); err != nil {
+				continue
 			}
-		case ObjectList, ObjectHash:
-			if err := kv.txn.Destory(obj, key); err != nil {
-				return -1, err
+			if obj.ExpireAt > now {
+				if err := unExpireAt(kv.txn, keys[i], obj.ExpireAt); err != nil {
+					return count, err
+				}
 			}
+			count++
 		}
-		n++
 	}
-	return n, nil
+	return count, nil
 }
 
 // ExpireAt set a timeout on key
 func (kv *Kv) ExpireAt(key []byte, at int64) error {
 	mkey := MetaKey(kv.txn.db, key)
+	now := Now()
+
 	meta, err := kv.txn.t.Get(mkey)
 	if err != nil {
 		if IsErrNotFound(err) {
@@ -77,43 +96,50 @@ func (kv *Kv) ExpireAt(key []byte, at int64) error {
 		}
 		return err
 	}
-	obj := &Object{}
-	err = json.Unmarshal(meta, obj)
+	obj, err := DecodeObject(meta)
 	if err != nil {
 		return err
 	}
-	var updated []byte
-	switch obj.Type {
-	case ObjectList:
-		listMeta := &ListMeta{}
-		if err := json.Unmarshal(meta, listMeta); err != nil {
-			return err
-		}
-		listMeta.ExpireAt = at
-		updated, err = json.Marshal(listMeta)
-		if err != nil {
-			return err
-		}
-	case ObjectString:
-		strMeta := &StringMeta{}
-		if err := json.Unmarshal(meta, strMeta); err != nil {
-			return err
-		}
-		strMeta.ExpireAt = at
-		updated, err = json.Marshal(strMeta)
-		if err != nil {
-			return err
-		}
-	case ObjectSet:
-	case ObjectZset:
-	case ObjectHash:
+	if IsExpired(obj, now) {
+		return ErrKeyNotFound
 	}
-
 	if err := expireAt(kv.txn, key, obj.ID, obj.ExpireAt, at); err != nil {
 		return err
 	}
-
+	obj.ExpireAt = at
+	updated := EncodeObject(obj)
+	updated = append(updated, meta[ObjectEncodingLength:]...)
 	return kv.txn.t.Set(mkey, updated)
+}
+
+//Exists
+func (kv *Kv) Exists(keys [][]byte) (int64, error) {
+	var (
+		now   int64    = Now()
+		count int64    = 0
+		mkeys [][]byte = make([][]byte, len(keys))
+	)
+	for i, key := range keys {
+		mkeys[i] = MetaKey(kv.txn.db, key)
+	}
+
+	dataBytes, err := store.BatchGetValues(kv.txn.t, mkeys)
+	if err != nil {
+		return count, err
+	}
+	for i, data := range dataBytes {
+		if data != nil {
+			obj, err := DecodeObject(data)
+			if err != nil {
+				return count, err
+			}
+			if IsExpired(obj, now) {
+				continue
+			}
+			count++
+		}
+	}
+	return count, nil
 }
 
 // FlushDB clear current db. FIXME one txn is limited for number of entries
@@ -155,4 +181,5 @@ func (kv *Kv) FlushAll() error {
 		}
 	}
 	return nil
+
 }

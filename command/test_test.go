ad73bf208178bbc0a9ce79ad1e433a21047f3c89
王鸿佳 <whj1@meitu.com>
2018-11-07 20:34:00 +0800

    test cae

---
+++ command/test_test.go
@@ -0,0 +1,42 @@
+package command
+
+import (
+	"bytes"
+	"io"
+
+	"gitlab.meitu.com/platform/thanos/conf"
+	"gitlab.meitu.com/platform/thanos/context"
+	"gitlab.meitu.com/platform/thanos/db"
+	"gitlab.meitu.com/platform/thanos/db/store"
+)
+
+var cfg = &conf.Tikv{
+	PdAddrs: store.MockAddr,
+}
+var mockdb *db.RedisStore
+
+func init() {
+	mockdb, _ = db.Open(cfg)
+}
+
+func ContextTest(name string, args ...string) *Context {
+	cliCtx := &context.ClientContext{
+		DB: mockdb.DB("defalut", 1),
+	}
+	servCtx := &context.ServerContext{
+		RequirePass: "",
+		Store:       mockdb,
+	}
+	rootCtx, _ := context.WithCancel(context.New(cliCtx, servCtx))
+	return &Context{
+		Name:    name,
+		Args:    args,
+		In:      &bytes.Buffer{},
+		Out:     &bytes.Buffer{},
+		Context: rootCtx,
+	}
+}
+
+func ctxString(buf io.Writer) string {
+	return buf.(*bytes.Buffer).String()
+}

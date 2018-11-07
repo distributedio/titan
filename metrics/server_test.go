f99b8213b915194452ec509e988e0cba5cdd5668
王鸿佳 <whj1@meitu.com>
2018-11-07 11:40:37 +0800

    add status unit test

---
+++ metrics/server_test.go
@@ -0,0 +1,34 @@
+package metrics
+
+import (
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"gitlab.meitu.com/platform/thanos/conf"
+)
+
+var (
+	cstatus = &conf.Status{
+		Listen: ":12345",
+	}
+)
+
+func TestListenAndServer(t *testing.T) {
+	server := NewServer(cstatus)
+	assert.NotNil(t, server)
+	go server.ListenAndServe(cstatus.Listen)
+	err := server.Stop()
+	assert.NoError(t, err)
+}
+
+func TestServer(t *testing.T) {
+	server := NewServer(cstatus)
+	assert.NotNil(t, server)
+	lis, err := net.Listen("tcp", cstatus.Listen)
+	assert.NoError(t, err)
+	go server.Serve(lis)
+	err = server.GracefulStop()
+	assert.NoError(t, err)
+}

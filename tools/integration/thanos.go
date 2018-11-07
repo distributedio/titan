ec799b18c902f43dc5e6ee92969f64e1ded7ec2f
王鸿佳 <whj1@meitu.com>
2018-11-07 10:05:39 +0800

    add function note

--- tools/integration/thanos.go
+++ tools/integration/thanos.go
@@ -22,14 +22,20 @@ var (
 		},
 	}
 
+	//ServerAddr default server addr
 	ServerAddr = "127.0.0.1:17369"
 	lis        net.Listener
 )
 
+//SetAuth default no verify
+// specify auth to enable validation
 func SetAuth(auth string) {
 	cfg.Auth = auth
 }
 
+//Start start server
+//1.open db
+//2.start server fd
 func Start() {
 	var err error
 	store, err := db.Open(&cfg.Tikv)
@@ -47,6 +53,7 @@ func Start() {
 	}
 }
 
+//Close close server listen fd
 func Close() {
 	if err := svr.Stop(); err != nil {
 		fmt.Println(err)

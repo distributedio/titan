096589b3cdb619a9d50ee815c019be353e2e90d4
王鸿佳 <whj1@meitu.com>
2018-11-08 15:08:10 +0800

    modify string

--- db/string.go
+++ db/string.go
@@ -1,9 +1,5 @@
 package db
 
-import (
-	"encoding/json"
-)
-
 type StringMeta struct {
 	Object
 	Value []byte
@@ -17,64 +13,106 @@ type String struct {
 
 func GetString(txn *Transaction, key []byte) (*String, error) {
 	str := &String{txn: txn, key: key}
+	now := Now()
 
 	mkey := MetaKey(txn.db, key)
 	meta, err := txn.t.Get(mkey)
 	if err != nil {
 		if IsErrNotFound(err) {
-			now := Now()
 			str.meta.CreatedAt = now
 			str.meta.UpdatedAt = now
 			str.meta.ExpireAt = 0
-			str.meta.ID = key
+			str.meta.ID = UUID()
 			str.meta.Type = ObjectString
 			str.meta.Encoding = ObjectEncodingRaw
 			return str, nil
 		}
 		return nil, err
 	}
-	if err := json.Unmarshal(meta, &str.meta); err != nil {
+	if err := str.decode(meta); err != nil {
 		return nil, err
 	}
+
+	if str.meta.Type != ObjectString {
+		return nil, ErrTypeMismatch
+	}
+
 	if str.meta.Encoding != ObjectEncodingRaw {
 		return nil, ErrTypeMismatch
 	}
+
+	str.meta.UpdatedAt = now
+	str.meta.ExpireAt = 0
 	return str, nil
 }
 
+//NewString  create new string object
+func NewString(txn *Transaction, key []byte) *String {
+	str := &String{txn: txn, key: key}
+	now := Now()
+	str.meta.CreatedAt = now
+	str.meta.UpdatedAt = now
+	str.meta.ExpireAt = 0
+	str.meta.ID = UUID()
+	str.meta.Type = ObjectString
+	str.meta.Encoding = ObjectEncodingRaw
+	return str
+}
+
+//Gets the value information for the key from db
 func (s *String) Get() ([]byte, error) {
-	if len(s.meta.Value) == 0 {
+	if !s.Exist() {
 		return nil, ErrKeyNotFound
 	}
 	return s.meta.Value, nil
 }
 
-func (s *String) Set(val []byte, at ...int64) error {
-	if len(at) != 0 {
+func (s *String) Set(val []byte, expire ...int64) error {
+	timestamp := Now()
+	if len(expire) != 0 {
 		old := s.meta.ExpireAt
-		s.meta.ExpireAt = at[0]
-		if err := expireAt(s.txn, s.key, s.key, old, at[0]); err != nil {
+		s.meta.ExpireAt = timestamp + expire[0]
+		if err := expireAt(s.txn, s.key, s.key, old, s.meta.ExpireAt); err != nil {
 			return err
 		}
 	}
 	s.meta.Value = val
-	return s.updateMeta()
+	return s.txn.t.Set(MetaKey(s.txn.db, s.key), s.encode())
 }
 
+//Len value len
 func (s *String) Len() (int, error) {
 	return len(s.meta.Value), nil
 }
 
-func (s *String) updateMeta() error {
-	meta, err := json.Marshal(s.meta)
-	if err != nil {
-		return err
-	}
-	return s.txn.t.Set(MetaKey(s.txn.db, s.key), meta)
-}
+//Exist return ture if key exist
 func (s *String) Exist() bool {
 	if s.meta.Value == nil {
 		return false
 	}
 	return true
+}
+
+func (s *String) encode() []byte {
+	b := EncodeObject(&s.meta.Object)
+	b = append(b, s.meta.Value...)
+	return b
+}
+
+func (s *String) decode(b []byte) error {
+	obj, err := DecodeObject(b)
+	if err != nil {
+		return err
+	}
+
+	timestamp := Now()
+	if obj.ExpireAt != 0 && obj.ExpireAt < timestamp {
+		return nil
+	}
+
+	s.meta.Object = *obj
+	if len(b) > ObjectEncodingLength {
+		s.meta.Value = b[ObjectEncodingLength:]
+	}
+	return nil
 }
---
+++ db/string_test.go
@@ -0,0 +1,113 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func EqualGet(t *testing.T, db *DB, key []byte, value []byte) {
+	txn, _ := db.Begin()
+	obj, err := GetString(txn, key)
+	assert.NoError(t, err)
+	v, err := obj.Get()
+	assert.Equal(t, value, v)
+	assert.NoError(t, err)
+	txn.Commit(context.TODO())
+}
+
+func EqualNotFound(t *testing.T, db *DB, key []byte) {
+	// 不存在的key ，判断错误类型
+	txn, _ := db.Begin()
+	obj, err := GetString(txn, key)
+	assert.NoError(t, err)
+	v, err := obj.Get()
+	assert.Nil(t, v)
+	assert.Equal(t, false, obj.Exist())
+	txn.Commit(context.TODO())
+}
+
+/*
+func TestStringSet(t *testing.T) {
+	value := []byte("value")
+	key := []byte("string-key")
+
+	db := MockDB()
+	txn, _ := db.Begin()
+	obj, err := GetString(txn, key)
+	assert.NoError(t, err)
+	err = obj.Set(value)
+	assert.NoError(t, err)
+	txn.Commit(context.TODO())
+	EqualGet(t, db, key, value)
+}
+
+/*
+//过期校验
+func TestStringSetPx(t *testing.T) {
+	value := []byte("value-px")
+	key := []byte("key-px")
+
+	db := MockDB()
+	txn, _ := db.Begin()
+	obj, err := GetString(txn, key)
+	assert.NoError(t, err)
+
+	err = obj.Set(value, int64(time.Millisecond*100))
+	assert.NoError(t, err)
+	txn.Commit(context.TODO())
+	EqualGet(t, db, key, value)
+	time.Sleep(time.Second)
+	EqualNotFound(t, db, key)
+}
+
+
+//过期校验
+func TestStringSetExpire(t *testing.T) {
+	value := []byte("value-ex")
+	key := []byte("key-ex")
+
+	db := MockDB()
+	txn, _ := db.Begin()
+	obj, err := GetString(txn, key)
+	assert.NoError(t, err)
+
+	err = obj.Set(value, int64(time.Second))
+	assert.NoError(t, err)
+	txn.Commit(context.TODO())
+	EqualGet(t, db, key, value)
+	time.Sleep(time.Second)
+	EqualNotFound(t, db, key)
+}
+
+/*
+func TestStrings(t *testing.T) {
+	keys := make([][]byte, 5)
+	keys[0] = []byte("key")
+	keys[1] = []byte("k2")
+	keys[2] = []byte("key-ex")
+	keys[3] = []byte("k3")
+	keys[4] = []byte("key-px")
+	value := []byte("value")
+
+	db.Begin()
+	objs, err := db.Strings(keys)
+
+	assert.Len(t, keys, len(objs))
+	assert.NoError(t, err)
+
+	err = objs[3].Set(value, 0, 1)
+	assert.NoError(t, err)
+
+	err = objs[0].Set(value, 0, 1)
+	assert.NoError(t, err)
+	db.Commit()
+
+	EqualGet(t, keys[0], value)
+	EqualNotFound(t, keys[1])
+	EqualNotFound(t, keys[2])
+	EqualGet(t, keys[3], value)
+	EqualNotFound(t, keys[4])
+}
+*/

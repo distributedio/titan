9d4e3c6c276414bc0fb0cf63a8690c26d611a984
焦龙 <jiaolong@meitu.com>
2018-11-12 19:31:37 +0800

    add test

---
+++ db/db_test.go
@@ -0,0 +1,23 @@
+package db
+
+import (
+	"os"
+	"testing"
+
+	"github.com/pingcap/tidb/store/mockstore"
+)
+
+var mockDB *DB
+
+func TestMain(m *testing.M) {
+	store, err := mockstore.NewMockTikvStore()
+	if err != nil {
+		panic(err)
+	}
+	mockDB = &DB{
+		Namespace: "mockdb-ns",
+		ID:        1,
+		kv:        &RedisStore{store},
+	}
+	os.Exit(m.Run())
+}

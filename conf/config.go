c1ce1328acff63cd0853aac7204dbb586fed79af
任勇全 <ryq@meitu.com>
2018-11-13 19:00:44 +0800

    Refactor codes about logger

--- conf/config.go
+++ conf/config.go
@@ -36,7 +36,7 @@ type ZT struct {
 //Logger config is the config of default zap log
 type Logger struct {
 	Name       string `cfg:"name; titan; ; the default logger name"`
-	Path       string `cfg:"path; logs/titan.log; ; the default log path"`
+	Path       string `cfg:"path; logs/titan; ; the default log path"`
 	Level      string `cfg:"level; info; ; log level(debug, info, warn, error, panic, fatal)"`
 	Compress   bool   `cfg:"compress; false; boolean; true for enabling log compress"`
 	TimeRotate string `cfg:"time-rotate; 0 0 0 * * *; ; log time rotate pattern(s m h D M W)"`
@@ -44,7 +44,7 @@ type Logger struct {
 
 //TikvLogger config is the config of tikv log
 type TikvLogger struct {
-	Path       string `cfg:"path; logs/tikv.log;nonempty ; the default log path"`
+	Path       string `cfg:"path; logs/tikv;nonempty ; the default log path"`
 	Level      string `cfg:"level; info; ; log level(debug, info, warn, error, panic, fatal)"`
 	Compress   bool   `cfg:"compress; false; boolean; true for enabling log compress"`
 	TimeRotate string `cfg:"time-rotate; 0 0 0 * * *; ; log time rotate pattern(s m h D M W)"`

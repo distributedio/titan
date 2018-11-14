968ad5a3ee88fd8dfd8132c3216ef7b751c8f131
王鸿佳 <whj1@meitu.com>
2018-11-14 11:14:43 +0800

    del test log

--- db/test_test.go
+++ db/test_test.go
@@ -2,9 +2,11 @@ package db
 
 import (
 	"github.com/pingcap/tidb/store/mockstore"
+	"go.uber.org/zap"
 )
 
 func MockDB() *DB {
+	zap.ReplaceGlobals(zap.NewNop())
 	store, err := mockstore.NewMockTikvStore()
 	if err != nil {
 		panic(err)

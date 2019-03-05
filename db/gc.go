1ab28ee520b726d735962881abd2defdac23ff62
zhs <zhs1@meitu.com>
2019-03-05 18:52:59 +0800

    add debug log

--- db/gc.go
+++ db/gc.go
@@ -26,7 +26,9 @@ func toTikvGCKey(key []byte) []byte {
 // {sys.ns}:{sys.id}:{GC}:{prefix}
 // prefix: {user.ns}:{user.id}:{M/D}:{user.objectID}
 func gc(txn store.Transaction, prefix []byte) error {
-	zap.L().Debug("add to gc", zap.ByteString("prefix", prefix))
+	if logEnv := zap.L().Check(zap.DebugLevel, "[GC] add to gc"); logEnv != nil {
+		logEnv.Write(zap.ByteString("prefix", prefix))
+	}
 	metrics.GetMetrics().GCKeysCounterVec.WithLabelValues("gc_add").Inc()
 	return txn.Set(toTikvGCKey(prefix), []byte{0})
 }
@@ -59,7 +61,10 @@ func doGC(db *DB, limit int) error {
 	gcPrefix := toTikvGCKey(nil)
 	dbTxn, err := db.Begin()
 	if err != nil {
-		zap.L().Error("[GC] transection begin failed", zap.Error(err))
+		zap.L().Error("[GC] transection begin failed",
+			zap.ByteString("gcprefix", gcPrefix),
+			zap.Int("limit", limit),
+			zap.Error(err))
 		return err
 	}
 	txn := dbTxn.t
@@ -71,22 +76,29 @@ func doGC(db *DB, limit int) error {
 	}
 	defer itr.Close()
 	if !itr.Valid() || !itr.Key().HasPrefix(gcPrefix) {
-		zap.L().Debug("[GC] no gc item")
+		if logEnv := zap.L().Check(zap.DebugLevel, "[GC] not need to gc item"); logEnv != nil {
+			logEnv.Write(zap.ByteString("gcprefix", gcPrefix), zap.Int("limit", limit))
+		}
 		return nil
 	}
 	gcKeyCount := 0
 	dataKeyCount := 0
 	for itr.Valid() && itr.Key().HasPrefix(gcPrefix) {
-		prefix := itr.Key()[len(gcPrefix):]
+		dataPrefix := itr.Key()[len(gcPrefix):]
 		count := 0
-		zap.L().Debug("[GC] start to delete prefix", zap.String("prefix", string(prefix)), zap.Int("limit", limit))
-		if count, err = gcDeleteRange(txn, prefix, limit); err != nil {
+		if logEnv := zap.L().Check(zap.DebugLevel, "[GC] start to delete prefix"); logEnv != nil {
+			logEnv.Write(zap.ByteString("data-prefix", dataPrefix), zap.Int("limit", limit))
+		}
+		if count, err = gcDeleteRange(txn, dataPrefix, limit); err != nil {
 			return err
 		}
 
 		//check and delete gc key
 		if limit > 0 && count < limit || limit <= 0 && count > 0 {
-			zap.L().Debug("[GC] delete prefix succeed", zap.String("prefix", string(prefix)))
+			if logEnv := zap.L().Check(zap.DebugLevel, "[GC] delete prefix succeed"); logEnv != nil {
+				logEnv.Write(zap.ByteString("data-prefix", dataPrefix), zap.Int("limit", limit))
+			}
+
 			if err := txn.Delete(itr.Key()); err != nil {
 				txn.Rollback()
 				return err
@@ -99,7 +111,7 @@ func doGC(db *DB, limit int) error {
 			break
 		}
 		if err := itr.Next(); err != nil {
-			zap.L().Error("[GC] iter prefix err", zap.String("prefix", string(prefix)), zap.Error(err))
+			zap.L().Error("[GC] iter prefix err", zap.ByteString("data-prefix", dataPrefix), zap.Error(err))
 			break
 		}
 	}
@@ -108,8 +120,11 @@ func doGC(db *DB, limit int) error {
 		txn.Rollback()
 		return err
 	}
-
-	zap.L().Debug("[GC] txn commit success", zap.Int("gcKey", gcKeyCount), zap.Int("dataKey", dataKeyCount))
+	if logEnv := zap.L().Check(zap.DebugLevel, "[GC]  txn commit success"); logEnv != nil {
+		logEnv.Write(zap.Int("limit", limit),
+			zap.Int("gcKeyCount", gcKeyCount),
+			zap.Int("dataKeyCount", dataKeyCount))
+	}
 	metrics.GetMetrics().GCKeysCounterVec.WithLabelValues("data_delete").Add(float64(dataKeyCount))
 	metrics.GetMetrics().GCKeysCounterVec.WithLabelValues("gc_delete").Add(float64(gcKeyCount))
 	return nil
@@ -125,15 +140,27 @@ func StartGC(db *DB, conf *conf.GC) {
 	for range ticker.C {
 		isLeader, err := isLeader(db, sysGCLeader, id, conf.LeaderLifeTime)
 		if err != nil {
-			zap.L().Error("[GC] check GC leader failed", zap.Error(err))
+			zap.L().Error("[GC] check GC leader failed",
+				zap.ByteString("leader", sysGCLeader),
+				zap.ByteString("uuid", id),
+				zap.Duration("leader-life-time", conf.LeaderLifeTime),
+				zap.Error(err))
 			continue
 		}
 		if !isLeader {
-			zap.L().Debug("[GC] not GC leader")
+			if logEnv := zap.L().Check(zap.DebugLevel, "[GC]  current is not gc leader"); logEnv != nil {
+				logEnv.Write(zap.ByteString("leader", sysGCLeader),
+					zap.ByteString("uuid", id),
+					zap.Duration("leader-life-time", conf.LeaderLifeTime))
+			}
 			continue
 		}
 		if err := doGC(db, conf.BatchLimit); err != nil {
-			zap.L().Error("[GC] do GC failed", zap.Error(err))
+			zap.L().Error("[GC] do GC failed",
+				zap.ByteString("leader", sysGCLeader),
+				zap.ByteString("uuid", id),
+				zap.Duration("leader-life-time", conf.LeaderLifeTime),
+				zap.Error(err))
 			continue
 		}
 	}

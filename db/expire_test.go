4a2053e2c1b4e009fe2ae29d39b7e61f20475d73
zhs <zhs1@meitu.com>
2019-04-26 20:19:46 +0800

    remove blank line

--- db/expire_test.go
+++ db/expire_test.go
@@ -188,7 +188,6 @@ func Test_doExpire(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-
 			txn := getTxn(t)
 			err := doExpire(txn, tt.args.mkey, tt.args.id)
 			txn.Commit(context.TODO())

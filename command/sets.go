124b77d463dd277508b918810d0b7489db6b40d8
piaoairy219 <airy9801@163.com>
2019-06-05 17:30:15 +0800

    spop key m1 m2 or spop key 1 2 will report syntax error, which is same with redis-server

--- command/sets.go
+++ command/sets.go
@@ -76,12 +76,15 @@ func SPop(ctx *Context, txn *db.Transaction) (OnCommit, error) {
 	count := 1
 	var err error
 	key := []byte(ctx.Args[0])
-	if len(ctx.Args) >= 2 {
+	if len(ctx.Args) == 2 {
 		count, err = strconv.Atoi(ctx.Args[1])
 		if err != nil {
-			return nil, ErrInteger
+			return nil, ErrSyntax
 		}
+	} else if len(ctx.Args) > 2 {
+		return nil, ErrSyntax
 	}
+
 	set, err := txn.Set(key)
 	if err != nil {
 		return nil, errors.New("ERR " + err.Error())

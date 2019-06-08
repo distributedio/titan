574d98a50781830ca50debb2e53ff9cf8f3b91b8
Tim Vaillancourt <timvaillancourt@github.com>
2019-06-09 01:12:02 +0200

    Additional test

--- bin/titan/main_test.go
+++ bin/titan/main_test.go
@@ -13,15 +13,23 @@ const (
 )
 
 func TestGetTLSServerOpts(t *testing.T) {
-	_, err := getTLSServerOpts("notfound", "notfoundeither")
+	// not found
+	_, err := getTLSServerOpts("notfound", keyFile)
 	assert.Error(t, err)
 
+	// not found #2
+	_, err = getTLSServerOpts(certFile, "notfound")
+	assert.Error(t, err)
+
+	// broken file #1
 	_, err = getTLSServerOpts(brokenFile, keyFile)
 	assert.Error(t, err)
 
+	// broken file #2
 	_, err = getTLSServerOpts(certFile, brokenFile)
 	assert.Error(t, err)
 
+	// success
 	opts, err := getTLSServerOpts(certFile, keyFile)
 	assert.NoError(t, err)
 	assert.NotNil(t, opts)

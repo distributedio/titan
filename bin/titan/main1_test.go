08c915cd5967aa2f3144b0a332080d3df46731a8
Don Bowman <don@agilicus.com>
2020-04-08 17:38:09 -0400

    fix: add additional log writer UT

--- bin/titan/main1_test.go
+++ bin/titan/main1_test.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"io/ioutil"
 	"os"
+	"path"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,4 +17,13 @@ func TestWriter(t *testing.T) {
 	stream, _err = Writer("stderr", "", true)
 	assert.Equal(t, stream, os.Stderr)
 	assert.Nil(t, _err)
+
+	td, td_err := ioutil.TempDir("", "titan-test")
+	assert.Nil(t, td_err)
+	stream, _err = Writer(path.Join(td, "titan-test-log"), "* * * * *", true)
+	assert.Nil(t, _err)
+
+	stream, _err = Writer(path.Join(td, "titan-test-log"), "", true)
+	assert.NotNil(t, _err)
+
 }
